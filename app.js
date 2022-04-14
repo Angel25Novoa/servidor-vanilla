e224df6a379b847c427c2565ff10ac7c75498d29
Angel25Novoa <jorge.250495@gmail.com>
2022-04-14 18:53:38 -0500

    express

--- app.js
+++ app.js
@@ -4,30 +4,38 @@ import express from 'express'
 
 const PORT = 3080
 const expressApp = express()
+expressApp.use(express.json())
+expressApp.use(express.text())
 
-expressApp.listen(3000, () => {
+expressApp.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
 
-expressApp.get('/saludar', (req, res) => {
-    res.send('Hola mundo') 
+expressApp.post('/cuenta', (req, res) => {
+    console.log(req.body)
+    res.send()
 })
 
-const httpServer = createServer((req, res) => {
-    // console.log(req.method, req.url, req.headers)
-    let data = ''
-    let chunkIndex = 0
-    req.on('data', (chunk) => {
-        data += chunk
-        chunkIndex++
-        console.log(chunkIndex)
-    })
-    req.on('end', () => {
-        console.log(data)
-        res.end('Hello World')
-    })
-
+expressApp.put('/producto', (req, res) => {
+    console.log(req.body)
+    res.send()
 })
+
+// const httpServer = createServer((req, res) => {
+//     // console.log(req.method, req.url, req.headers)
+//     let data = ''
+//     let chunkIndex = 0
+//     req.on('data', (chunk) => {
+//         data += chunk
+//         chunkIndex++
+//         console.log(chunkIndex)
+//     })
+//     req.on('end', () => {
+//         console.log(data)
+//         res.end('Hello World')
+//     })
+
+// })
 
 // httpServer.listen(3000)
 
